8f726ec6dd1811df63f12d7a753b63d55fce3de1
nayciyilmaz <nayciyilmaz1@gmail.com>
2024-08-17 11:52:21 +0300

    first commit

---
+++ app/src/main/java/com/nayciyilmaz/artbookjava/Art.java
@@ -0,0 +1,12 @@
+package com.nayciyilmaz.artbookjava;
+
+public class Art {
+
+    String name;
+    int id;
+
+    public Art(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}
